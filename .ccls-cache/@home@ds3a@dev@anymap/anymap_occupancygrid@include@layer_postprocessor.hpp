f93ed93d488f64fab3db9d030ea1a71efb45109d
DS3a <33351122+DS3a@users.noreply.github.com>
2023-04-06 23:33:31 +0530

    make inflation to get 0.5x vehicle gap

--- .ccls-cache/@home@ds3a@dev@anymap/anymap_occupancygrid@include@layer_postprocessor.hpp
+++ .ccls-cache/@home@ds3a@dev@anymap/anymap_occupancygrid@include@layer_postprocessor.hpp
@@ -64,12 +64,12 @@ namespace layer_postprocessor {
                   cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3)));
         this->image = image_eroded_with_3x3_kernel;*/
         cv::dilate(this->image, image_eroded_with_3x3_kernel,
-                  cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(15, 15)));
+                  cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(61, 61)));
         this->image = image_eroded_with_3x3_kernel;
-        cv::dilate(this->image, image_eroded_with_3x3_kernel,
+/*        cv::dilate(this->image, image_eroded_with_3x3_kernel,
                   cv::getStructuringElement(cv::MORPH_RECT, cv::Size(29, 29)));
         this->image = image_eroded_with_3x3_kernel;
-
+*/
 
 
         this->back_to_grid();
