b8e4a99070f74c26e267e9d7cc6762c2318924dc
DS3a <33351122+DS3a@users.noreply.github.com>
2023-04-13 01:24:50 +0530

    update the inflation after testing

--- anymap_occupancygrid/include/layer_postprocessor.hpp
+++ anymap_occupancygrid/include/layer_postprocessor.hpp
@@ -67,7 +67,7 @@ namespace layer_postprocessor {
                   cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3)));
         this->image = image_eroded_with_3x3_kernel;
 */        cv::dilate(this->image, image_eroded_with_3x3_kernel,
-                   cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(61, 61)));
+                   cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(55, 55)));
         this->image = image_eroded_with_3x3_kernel;
 
 
