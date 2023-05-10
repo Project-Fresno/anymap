2ad334a297c47d1249aa55f6262bc08f050a168f
DS3a <33351122+DS3a@users.noreply.github.com>
2023-05-10 14:27:10 +0530

    increase the leaf size for lane pcl

--- anymap_occupancygrid/src/anymap_node.cpp
+++ anymap_occupancygrid/src/anymap_node.cpp
@@ -236,7 +236,7 @@ void AnyMapNode::lanes_callback(const sensor_msgs::msg::PointCloud2::SharedPtr m
     pcl::VoxelGrid<POINT_TYPE> sor;
 
     sor.setInputCloud(this->lanes_cloud);
-    sor.setLeafSize(0.18f, 0.18f, 0.18f);
+    sor.setLeafSize(0.28f, 0.28f, 0.28f);
     sor.filter(*lanes_filtered);
     this->lanes_source_ptr->set_point_weight(0.6);
 
