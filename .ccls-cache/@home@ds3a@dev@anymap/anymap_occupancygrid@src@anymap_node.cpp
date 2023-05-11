dbbcd14927d70c612c333fbc2fff23b9cff4dfbd
DS3a <33351122+DS3a@users.noreply.github.com>
2023-05-11 17:13:49 +0530

    change leaf size in z

--- .ccls-cache/@home@ds3a@dev@anymap/anymap_occupancygrid@src@anymap_node.cpp
+++ .ccls-cache/@home@ds3a@dev@anymap/anymap_occupancygrid@src@anymap_node.cpp
@@ -236,7 +236,7 @@ void AnyMapNode::lanes_callback(const sensor_msgs::msg::PointCloud2::SharedPtr m
     pcl::VoxelGrid<POINT_TYPE> sor;
 
     sor.setInputCloud(this->lanes_cloud);
-    sor.setLeafSize(0.28f, 0.28f, 0.28f);
+    sor.setLeafSize(0.28f, 0.28f, 1.28f);
     sor.filter(*lanes_filtered);
     this->lanes_source_ptr->set_point_weight(0.6);
 
