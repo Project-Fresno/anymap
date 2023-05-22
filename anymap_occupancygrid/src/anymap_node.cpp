4d4e7bdef84e7f39bec48c4e531e48c3c75ec3c9
DS3a <33351122+DS3a@users.noreply.github.com>
2023-05-23 01:04:20 +0530

    increase lidar inflation firther

--- anymap_occupancygrid/src/anymap_node.cpp
+++ anymap_occupancygrid/src/anymap_node.cpp
@@ -164,7 +164,7 @@ AnyMapNode::AnyMapNode() : Node("anymap_node") {
         new observation_source::ObservationSource("lidar", this->anymap_ptr));
     this->lidar_postprocessor.set_layer_name("lidar");
     this->lidar_postprocessor.set_input_grid(this->anymap_ptr);
-    this->lidar_postprocessor.set_inflation(27, 27);
+    this->lidar_postprocessor.set_inflation(31, 31);
 
     pcl::PointCloud<POINT_TYPE>::Ptr temp_lidar_cloud (new pcl::PointCloud<POINT_TYPE>());
     this->lidar_cloud = temp_lidar_cloud;
