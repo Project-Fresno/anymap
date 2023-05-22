1550d1869c459c053e9a74e34b14d844843a3e76
DS3a <33351122+DS3a@users.noreply.github.com>
2023-05-23 04:48:00 +0530

    optimize

--- anymap_occupancygrid/src/anymap_node.cpp
+++ anymap_occupancygrid/src/anymap_node.cpp
@@ -239,9 +239,9 @@ void AnyMapNode::lanes_callback(const sensor_msgs::msg::PointCloud2::SharedPtr m
     pcl::VoxelGrid<POINT_TYPE> sor;
 
     sor.setInputCloud(this->lanes_cloud);
-    sor.setLeafSize(0.28f, 0.28f, 1.28f);
+    sor.setLeafSize(0.48f, 0.48f, 2.28f);
     sor.filter(*lanes_filtered);
-    this->lanes_source_ptr->set_point_weight(0.6);
+    this->lanes_source_ptr->set_point_weight(1.0);
 
     Eigen::Affine3f rs_transform = Eigen::Affine3f::Identity();
 
